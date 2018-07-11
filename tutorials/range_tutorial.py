5c85fe5b1db6772540112ebe4ed9b46c73a30359
Shashank Singh <shashank.singh1094@gmail.com>
2018-07-11 09:44:42 +0530

    range() tutorial

---
+++ tutorials/range_tutorial.py
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/477486/how-to-use-a-decimal-range-step-value
+
+print(list(range(9)))
+print(list(range(5, 9)))
+print(list(range(0, 9, 2)))
+print(list(range(9, 0, -2)))
+
+# print(list(range(0,9,0.5)))  # Range() doesn't take floating numbers
