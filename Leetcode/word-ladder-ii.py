4b0d21fe45b04b4b6097248af832ba9d3d106860
shashanksingh <shashanks@policybazaar.com>
2019-06-05 00:00:47 +0530

    fixes

--- Leetcode/word-ladder-ii.py
+++ Leetcode/word-ladder-ii.py
@@ -6,6 +6,8 @@ class Solution:
     def __init__(self):
         self.options = {}
         self.ans = []
+        self.first_found = False
+        self.min_len_yet = float("inf")
 
     def pre_computation(self, word_list):
         for word in word_list:
@@ -14,11 +16,12 @@ class Solution:
                     self.options[index] = set()
                 self.options[index].add(value)
 
-    def findLadders(self, beginWord: str, endWord: str, wordList):
-        self.pre_computation(wordList)
-        if endWord not in wordList:
+    def findLadders(self, beginWord: str, endWord: str, word_list):
+        self.pre_computation(word_list)
+        if endWord not in word_list:
             return self.ans
-        self.util(beginWord, endWord, wordList, [])
+        word_list = dict(zip(word_list, [True] * len(word_list)))
+        self.util(beginWord, endWord, word_list, [])
         # final_ans = []
         # min = float("inf")
         # for x in self.ans:
@@ -31,18 +34,36 @@ class Solution:
         curr_ans.append(a)
         if not word_list:
             return
+        if len(curr_ans) > self.min_len_yet:
+            return
         if a == z:
-            self.ans.append(curr_ans)
+            if not self.first_found:
+                self.first_found = True
+            if len(curr_ans) <= self.min_len_yet:
+                if len(curr_ans) < self.min_len_yet:
+                    self.min_len_yet = len(curr_ans)
+                    self.ans = []
+                self.ans.append(curr_ans)
         for x in range(len(a)):
-            for option in self.options[x]:
+            for option in self.options.get(x, []):
                 temp_a = a[:x] + option + a[x + 1:]
                 if temp_a in word_list:
                     temp_curr_ans = curr_ans.copy()
-                    temp_word_list = word_list.copy()
-                    temp_word_list.remove(temp_a)
-                    self.util(temp_a, z, temp_word_list, temp_curr_ans)
+                    temp_word_list_dict = word_list.copy()
+                    temp_word_list_dict.pop(temp_a)
+                    self.util(temp_a, z, temp_word_list_dict, temp_curr_ans)
 
 
 if __name__ == '__main__':
-    for x in Solution().findLadders("hit", "cog", ["hot","dot","dog","lot","log","cog"] ):
+    # for x in Solution().findLadders("qa", "sq",
+    #                                 ["si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le",
+    #                                  "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn",
+    #                                  "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc",
+    #                                  "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co",
+    #                                  "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an",
+    #                                  "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io",
+    #                                  "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"]):
+    #     print(x, end="\n\n")
+    for x in Solution().findLadders("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]):
         print(x, end="\n\n")
+    pass
